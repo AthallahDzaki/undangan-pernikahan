bc31e742dfaf76505615baac90eafca8e823d882
AthallahDzaki <AthallahDzaki@gmail.com>
2022-11-01 23:31:34 +0700

    Fix Error When Document Not Loaded

--- js/menikah.js
+++ js/menikah.js
@@ -39,7 +39,7 @@ $(document).ready(function($) {
   $("body").removeClass("preloader-site");
 });
 
-$(document).load(function() {
+window.onload = (event) => {
   var Body = $("body");
   Body.addClass("preloader-site");
-});
+};
